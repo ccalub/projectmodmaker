a0952c436d3d4655fb01fa66c01f8737fe5d5875
dabbo <dabbo@DESKTOP-65II4I0>
2022-01-20 20:02:19 -0500

    MCreator Merge commit '36f45778c92ae4fc41320a55387af8436402acea' into e97b076f2ac8671f44148cfc98888791ecb30998

--- src/main/java/net/mcreator/projectmodmaking/init/ProjectmodmakingModScreens.java
+++ src/main/java/net/mcreator/projectmodmaking/init/ProjectmodmakingModScreens.java
@@ -12,6 +12,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraft.client.gui.screens.MenuScreens;
 
 import net.mcreator.projectmodmaking.client.gui.BookofBeastsGUIScreen;
+import net.mcreator.projectmodmaking.client.gui.BookofBeastsGUI3Screen;
 import net.mcreator.projectmodmaking.client.gui.BookofBeastsGUI2Screen;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -21,6 +22,7 @@ public class ProjectmodmakingModScreens {
 		event.enqueueWork(() -> {
 			MenuScreens.register(ProjectmodmakingModMenus.BOOKOF_BEASTS_GUI, BookofBeastsGUIScreen::new);
 			MenuScreens.register(ProjectmodmakingModMenus.BOOKOF_BEASTS_GUI_2, BookofBeastsGUI2Screen::new);
+			MenuScreens.register(ProjectmodmakingModMenus.BOOKOF_BEASTS_GUI_3, BookofBeastsGUI3Screen::new);
 		});
 	}
 }
